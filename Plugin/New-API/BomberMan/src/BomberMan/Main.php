8c207804880a2fb2149e3328c64b4488719157d1
Philip Shilling <comedyman937@yahoo.com>
2014-06-06 16:42:08 -0500

    Basics

---
+++ Plugin/New-API/BomberMan/src/BomberMan/Main.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace BomberMan;
+
+use pocketmine\level\Level;
+//Other PocketMine uses
+use pocketmine\Player;
+
+class Main {
+    public function onEnable(){
+    }
+    
+    public function onDisable(){
+    }
+}
