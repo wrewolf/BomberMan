992d86bb2271fd3843e7c988757f2828c6f93934
Philip Shilling <comedyman937@yahoo.com>
2014-06-08 11:44:57 -0500

    0.6.7 - Fixed TNT Giving
    TNT was being given to everyone on the server when the game starts.  This makes it so only people in the BomberMan Level will be able to get TNT from the plugin.

--- Plugin/BomberMan.php
+++ Plugin/BomberMan.php
@@ -4,7 +4,7 @@
 __PocketMine Plugin__
 name=BomberMan
 description=Make a path using TNT and kill the others!
-version=0.6.6
+version=0.6.7
 author=Comedyman937
 class=BomberMan
 apiversion=11,12
@@ -14,6 +14,9 @@ apiversion=11,12
 ===============
    Changelog
 ===============
+0.6.7
+- Fixed TNT Giving
+
 0.6.6
 - Fixed Crashes
 
@@ -281,7 +284,9 @@ class BomberMan implements Plugin
             $this->BomberManActive = true;
         }
                 foreach($this->api->player->getAll() as $p){
-                    $this->api->console->run("give " . $p . " 46 6");
+                    if($p->level->getName() == $this->CONFIG["BomberManLevel"])
+                        $this->api->console->run("give " . $p . " 46 6");
+                    }
                 }
                        $this->api->chat->broadcast("[BomberMan] Gathering Level Name...");
                        $BomberManLevelName = $this->CONFIG["BomberManLevelName"];
